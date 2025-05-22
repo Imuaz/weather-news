d6553ebd72328eb8a18f4a82dece09a5e98cdd1b
Muazu Yakub Idriss <imuazyakub@gmail.com>
2025-05-22 10:48:52 +0100

    node setup

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+
+const app = express()
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: tru }));
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
